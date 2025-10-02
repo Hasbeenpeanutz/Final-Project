921276962a27fcbf4f94ee12df1d0e21383295f0
lyniqna <fitriani.widodo18@gmail.com>
2025-10-02 15:00:56 +0700

    append line

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hi, this is the first line");
         System.out.println("User 1 adds a new line");
         System.out.println("User 2 adds a new line");
+        System.out.println("User 3 adds a new line");
     }
 }
